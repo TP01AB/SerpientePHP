b007a01b5c19c225effb1b8172695752221ee669
israel <isra9movil@hotmail.com>
2020-11-03 12:45:05 +0100

    Creacion clase serpiente

---
+++ Serpiente.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Serpiente
+ *
+ * @author isra9
+ */
+class Serpiente {
+    //put your code here
+    
+    
+}
